43f2eb746ff7b5dffa70f0fcbbce0f3c82137fc1
Belgin <belgin.doan@gmail.com>
2023-02-04 10:39:34 -0500

    Cart, PaymentMethod and UserType classes are created.

--- UserType.java
+++ UserType.java
@@ -1,8 +1,9 @@
 package ShoppingApp;
 
-public class UserType {
+public enum UserType {
 
         /** This is an Enum class. It has 2 values
          * PREMIUM and REGULAR
         * */
+        PREMIUM, REGULAR
 }
