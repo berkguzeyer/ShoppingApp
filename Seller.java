527847e9259e701b3a858c78552a633d64d30491
mehmetAltinpinar <mehmetu7@gmail.com>
2023-02-04 10:43:56 -0500

    commit seller class

--- Seller.java
+++ Seller.java
@@ -2,6 +2,7 @@ package ShoppingApp;
 
 
 public class Seller{
+    private double balance;
 
 /** This is an abstract class and it inherits from User class. It has
  *                      private list of Product named products,
