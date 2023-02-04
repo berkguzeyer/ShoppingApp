d3f0c1dccf38551cdb8aa4a386a51fb1f5d313c1
azizh <azizhnagis@gmail.com>
2023-02-04 11:32:18 -0500

    mastercard added needs to be deleted

--- PaymentMethod.java
+++ PaymentMethod.java
@@ -6,5 +6,5 @@ public enum PaymentMethod {
     /** This is an Enum class. It has 2 values;
      *                  CREDITCARD and CASHonDELIVERY
      **/
-    CREDITCARD, CASHonDELIVERY
+    CREDITCARD, CASHonDELIVERY , MASTERCARD
 }
