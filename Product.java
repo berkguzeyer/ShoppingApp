bdd6f63c57cc41b4c1f73402c43da3e2e1d027ac
azizh <azizhnagis@gmail.com>
2023-02-08 18:18:40 -0500

    mastercard added needs to be deleted

--- Product.java
+++ Product.java
@@ -2,6 +2,49 @@ package ShoppingApp;
 
 public class Product {
 
+    private String name;
+    private double price;
+    private int stock;
+    private Seller seller;
+
+    public Product(String name, double price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
+
     /** This class has
      *              private String name;
      *              private double price;
